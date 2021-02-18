1415aa6e12777ce8de14772e3a1e0c93006c8022
Sha <cspoppuppy@hotmail.com>
2021-02-18 19:56:58 +0000

    add export to csv

--- directory.rb
+++ directory.rb
@@ -29,6 +29,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to student.csv"
   puts "9. Exit"
 end
 def show_students
@@ -42,11 +43,24 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "I don't know what you meant, try again"
   end
+end
+def save_students
+   # open the file for writing
+   file = File.open("students.csv", "w")
+   # iterate over the array of save_students
+   @students.each do |student|
+     student_data = [student[:name], student[:cohort]]
+     csv_line = student_data.join(",")
+     file.puts csv_line
+   end
+   file.close
 end
 
 def interactive_menu
