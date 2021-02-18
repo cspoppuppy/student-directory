f5e7251ad933bf1f14572efc8767b8dd97637701
Sha <cspoppuppy@hotmail.com>
2021-02-18 22:04:24 +0000

    flexible file name

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 def print_header
@@ -29,7 +29,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to student.csv"
+  puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
@@ -65,13 +65,24 @@ def save_students
    end
    file.close
 end
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+def try_load_students
+  filename = ARGV.first # first argument from the command csv_line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
 end
 
 def interactive_menu
@@ -79,8 +90,9 @@ def interactive_menu
     # 1. print the menu and ask the user what to do
     print_menu
     # 3. do what the user has asked
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
+try_load_students
 interactive_menu
